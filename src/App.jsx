8ba89b71866f5c69eec9adba1e4bee28b6f3dc31
TwinMine <michaelrolo11@gmail.com>
2026-07-21 11:49:12 +0200

    restart portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import {  RouterProvider } from "react-router-dom";
-import { router } from "./components/layouts/Router";
 
 function App() {
   
   return (
     <div className="app">
-      <RouterProvider router={router} />
+
     </div>
   );
 }
