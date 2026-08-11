a35fbb3a3bb0c0e94a060fbe92695f521725a0ea
TwinMine <michaelrolo11@gmail.com>
2026-08-11 10:05:44 +0200

    add disabled to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
   const navigate = useNavigate();
   const { appContext, setAppContext } = useContext(AppContext);
   const [openingApp, setOpeningApp] = useState(null);
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
   // const [counter, setCounter] = useState(0)
 
   const navArray = [
@@ -16,21 +16,25 @@ const Navigation = () => {
         name: "Über mich",
         path: "/about",
         icon: "faCircleInfo",
+        disabled: false,
       },
       {
         name: "Skills",
         path: "/skills",
         icon: "faScrewdriverWrench",
+        disabled: false,
       },
       {
         name: "Projekte",
         path: "/projects",
         icon: "faLaptop",
+        disabled: false,
       },
       {
         name: "Werdegang",
         path: "/resume",
         icon: "faBriefcase",
+        disabled: false,
       },
     ],
     [
@@ -38,31 +42,39 @@ const Navigation = () => {
         name: "Hobbies",
         path: "/hobbies",
         icon: "faGuitar",
+        disabled: false,
       },
       {
         name: "Kontakt",
         path: "/contact",
         icon: "faAt",
+        disabled: false,
       },
       {
         name: "Musik",
         path: "/music",
         icon: "faHeadphones",
+        disabled: true,
       },
       {
         name: "Einstellunge",
         path: "/settings",
         icon: "faGear",
+        disabled: true,
       },
     ],
   ];
 
   function openApp(item) {
+    if (!item || item.disabled) {
+      console.log("Die Function fehlt noch!");
+      return;
+    }
+
     setOpeningApp(item.path);
 
     setTimeout(() => {
       navigate(item.path);
-
       setAppContext(true);
     }, 500);
 
@@ -73,16 +85,23 @@ const Navigation = () => {
 
   return (
     <nav className={`device-app ${appContext ? "hide" : "app-open"}`}>
-      {counter > 0 ? <div className="navigation-button left"  onClick={() => setCounter(counter - 1)}>
-        <Icon iconName={"faCircleArrowLeft"}/>  
-        </div> : <></>}
+      {counter > 0 ? (
+        <div
+          className="navigation-button left"
+          onClick={() => setCounter(counter - 1)}
+        >
+          <Icon iconName={"faCircleArrowLeft"} />
+        </div>
+      ) : (
+        <></>
+      )}
       {navArray[counter].map((item) => (
         <div
           key={item.path}
           className={`screen-link ${openingApp === item.path ? "opening" : ""}`}
           onClick={() => openApp(item)}
         >
-          <div className="app-icon">
+          <div className={`app-icon ${item.disabled ? "disabled" : ""}`}>
             <Icon iconName={item.icon} />
           </div>
 
@@ -90,10 +109,13 @@ const Navigation = () => {
         </div>
       ))}
       {counter < navArray.length - 1 && (
-        <div className="navigation-button right" onClick={() => setCounter(counter + 1)}>
-        <Icon iconName={"faCircleArrowRight"}/>  
+        <div
+          className="navigation-button right"
+          onClick={() => setCounter(counter + 1)}
+        >
+          <Icon iconName={"faCircleArrowRight"} />
         </div>
-)}
+      )}
     </nav>
   );
 };
